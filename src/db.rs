d645b3ab30af9ef486347050192658bca88c795a
Prateek <104696532+pprateekk@users.noreply.github.com>
2026-01-03 20:15:15 -0500

    add custom error type and retrun Result from set()

--- src/db.rs
+++ src/db.rs
@@ -1,6 +1,13 @@
 use std::collections::HashMap;
 use std::sync::{Arc, RwLock};
 
+pub enum StashError {
+	EmptyKet, 
+	LockPoisoned,
+}
+
+pub type StashResult<T> = Result<T, StashError>;
+
 pub struct Stash {
 	pub store: Arc<RwLock<HashMap<String, String>>>,
 }
@@ -13,7 +20,14 @@ impl Stash {
 	}
 
 	pub fn set(&self, key: String, value: String) {
-		let mut store = self.store.write().unwrap();
+		if key.is_empty() {
+			return Err(StashError::EmptyKet);
+		}
+
+		let mut store = self.store.write().map_err(|_| StashError::LockPoisoned)?;
+
 		store.insert(key, value);
+
+		Ok(())
 	}
 }
